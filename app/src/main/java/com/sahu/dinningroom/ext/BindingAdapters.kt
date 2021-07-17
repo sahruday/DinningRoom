125df8cf94654e69b2f01822d7e3d40fd8314f96
sahru-8733 <sahruday.j@zohocorp.com>
2021-07-17 17:25:51 +0530

    Binding expressions included.

--- app/src/main/java/com/sahu/dinningroom/ext/BindingAdapters.kt
+++ app/src/main/java/com/sahu/dinningroom/ext/BindingAdapters.kt
@@ -8,20 +8,3 @@ import com.sahu.dinningroom.R
 fun atTime(view: TextView, time: String){
     view.text = view.resources.getString(R.string.atTime, time.getDisplayableDate())
 }
-
-@BindingAdapter("items")
-fun numberOfItems(view: TextView, count: Int){
-    view.text = view.resources.getString(R.string.itemCount, count.toString())
-}
-
-
-@BindingAdapter("color")
-fun colorAndTextOfItemIngredient(view:TextView, count: Int) {
-    view.text = count.toString()
-    view.setTextColor(view.resources.getColor(if(count<=5) R.color.red else R.color.green))
-}
-
-@BindingAdapter("quanity")
-fun quantityDisplay(view: TextView, count: Int) {
-    view.text = "x$count"//view.resources.getText(R.string.itemMultiplier, count.toString())
-}
